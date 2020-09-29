0df8c2678447c97d1fa3a0fd6f89c992889d6048
Eyal Cherevatzki <eyal.3555@protonmail.com>
2020-09-30 00:20:31 +0300

    style(web): 🎨 add Prettier config

--- web/scripts/setupTypeScript.js
+++ web/scripts/setupTypeScript.js
@@ -20,23 +20,28 @@ const { argv } = require("process")
 const projectRoot = argv[2] || path.join(__dirname, "..")
 
 // Add deps to pkg.json
-const packageJSON = JSON.parse(fs.readFileSync(path.join(projectRoot, "package.json"), "utf8"))
+const packageJSON = JSON.parse(
+  fs.readFileSync(path.join(projectRoot, "package.json"), "utf8")
+)
 packageJSON.devDependencies = Object.assign(packageJSON.devDependencies, {
   "svelte-check": "^1.0.0",
   "svelte-preprocess": "^4.0.0",
   "@rollup/plugin-typescript": "^6.0.0",
-  "typescript": "^3.9.3",
-  "tslib": "^2.0.0",
-  "@tsconfig/svelte": "^1.0.0"
+  typescript: "^3.9.3",
+  tslib: "^2.0.0",
+  "@tsconfig/svelte": "^1.0.0",
 })
 
 // Add script for checking
 packageJSON.scripts = Object.assign(packageJSON.scripts, {
-  "validate": "svelte-check"
+  validate: "svelte-check",
 })
 
 // Write the package JSON
-fs.writeFileSync(path.join(projectRoot, "package.json"), JSON.stringify(packageJSON, null, "  "))
+fs.writeFileSync(
+  path.join(projectRoot, "package.json"),
+  JSON.stringify(packageJSON, null, "  ")
+)
 
 // mv src/main.js to main.ts - note, we need to edit rollup.config.js for this too
 const beforeMainJSPath = path.join(projectRoot, "src", "main.js")
@@ -47,7 +52,7 @@ fs.renameSync(beforeMainJSPath, afterMainTSPath)
 const appSveltePath = path.join(projectRoot, "src", "App.svelte")
 let appFile = fs.readFileSync(appSveltePath, "utf8")
 appFile = appFile.replace("<script>", '<script lang="ts">')
-appFile = appFile.replace("export let name;", 'export let name: string;')
+appFile = appFile.replace("export let name;", "export let name: string;")
 fs.writeFileSync(appSveltePath, appFile)
 
 // Edit rollup config
@@ -55,9 +60,12 @@ const rollupConfigPath = path.join(projectRoot, "rollup.config.js")
 let rollupConfig = fs.readFileSync(rollupConfigPath, "utf8")
 
 // Edit imports
-rollupConfig = rollupConfig.replace(`'rollup-plugin-terser';`, `'rollup-plugin-terser';
+rollupConfig = rollupConfig.replace(
+  `'rollup-plugin-terser';`,
+  `'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';`)
+import typescript from '@rollup/plugin-typescript';`
+)
 
 // Replace name of entry point
 rollupConfig = rollupConfig.replace(`'src/main.js'`, `'src/main.ts'`)
@@ -68,22 +76,24 @@ let foundCSS = false
 let match
 
 // https://regex101.com/r/OtNjwo/1
-const configEditor = new RegExp(/css:.|\n*}/gmi)
-while (( match = configEditor.exec(rollupConfig)) != null) {
+const configEditor = new RegExp(/css:.|\n*}/gim)
+while ((match = configEditor.exec(rollupConfig)) != null) {
   if (foundCSS) {
     const endOfCSSIndex = match.index + 1
-    rollupConfig = rollupConfig.slice(0, endOfCSSIndex) + ",\n			preprocess: sveltePreprocess()," + rollupConfig.slice(endOfCSSIndex);
+    rollupConfig =
+      rollupConfig.slice(0, endOfCSSIndex) +
+      ",\n			preprocess: sveltePreprocess()," +
+      rollupConfig.slice(endOfCSSIndex)
     break
   }
   if (match[0].includes("css:")) foundCSS = true
 }
 
-
 // Add TypeScript
 rollupConfig = rollupConfig.replace(
-  'commonjs(),',
-  'commonjs(),\n\t\ttypescript({\n\t\t\tsourceMap: !production,\n\t\t\tinlineSources: !production\n\t\t}),'
-);
+  "commonjs(),",
+  "commonjs(),\n\t\ttypescript({\n\t\t\tsourceMap: !production,\n\t\t\tinlineSources: !production\n\t\t}),"
+)
 fs.writeFileSync(rollupConfigPath, rollupConfig)
 
 // Add TSConfig
@@ -93,7 +103,7 @@ const tsconfig = `{
   "include": ["src/**/*"],
   "exclude": ["node_modules/*", "__sapper__/*", "public/*"],
 }`
-const tsconfigPath =  path.join(projectRoot, "tsconfig.json")
+const tsconfigPath = path.join(projectRoot, "tsconfig.json")
 fs.writeFileSync(tsconfigPath, tsconfig)
 
 // Delete this script, but not during testing
@@ -103,8 +113,8 @@ if (!argv[2]) {
 
   // Check for Mac's DS_store file, and if it's the only one left remove it
   const remainingFiles = fs.readdirSync(path.join(__dirname))
-  if (remainingFiles.length === 1 && remainingFiles[0] === '.DS_store') {
-    fs.unlinkSync(path.join(__dirname, '.DS_store'))
+  if (remainingFiles.length === 1 && remainingFiles[0] === ".DS_store") {
+    fs.unlinkSync(path.join(__dirname, ".DS_store"))
   }
 
   // Check if the scripts folder is empty
@@ -116,13 +126,18 @@ if (!argv[2]) {
 
 // Adds the extension recommendation
 fs.mkdirSync(path.join(projectRoot, ".vscode"))
-fs.writeFileSync(path.join(projectRoot, ".vscode", "extensions.json"), `{
+fs.writeFileSync(
+  path.join(projectRoot, ".vscode", "extensions.json"),
+  `{
   "recommendations": ["svelte.svelte-vscode"]
 }
-`)
+`
+)
 
 console.log("Converted to TypeScript.")
 
 if (fs.existsSync(path.join(projectRoot, "node_modules"))) {
-  console.log("\nYou will need to re-run your dependency manager to get started.")
+  console.log(
+    "\nYou will need to re-run your dependency manager to get started."
+  )
 }
